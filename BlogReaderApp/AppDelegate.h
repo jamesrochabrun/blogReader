fadd5ab3d3373656501e2304fc283ddd9802cc14
james <jamesrochabrun@gmail.com>
2016-07-16 19:19:55 -0400

    Initial Commit

---
+++ BlogReaderApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlogReaderApp
+//
+//  Created by James Rochabrun on 16-07-16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlogReaderApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlogReaderApp
+//
+//  Created by James Rochabrun on 16-07-16.
+//  Copyright © 2016 James Rochabrun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
